2c5b220f05de052b076c7cb8f4a800799bea159d
I_AM_NOT_SPAM <95177138+kobitoDevelopment@users.noreply.github.com>
2025-03-14 12:45:21 +0900

    Delete src/app directory

--- src/app/components/test3/MyImage/MyImage.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-export default function MyImage() {
-  return (
-    <div>
-      {/* 最適化された画像を表示する作法としてImageコンポーネントを使用する */}
-      <Image src="/next.svg" alt="" width={200} height={200} quality={75} />
-    </div>
-  );
-}
