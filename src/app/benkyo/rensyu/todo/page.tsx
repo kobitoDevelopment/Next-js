71006464d3c1dea80b0e7db7abfb5e3d9a8cffff
I_AM_NOT_SPAM <95177138+kobitoDevelopment@users.noreply.github.com>
2025-04-01 00:04:13 +0900

    Update page.tsx

--- src/app/benkyo/rensyu/todo/page.tsx
+++ src/app/benkyo/rensyu/todo/page.tsx
@@ -1,20 +1,23 @@
 /*
-☑️ ToDoリストを保持するリアクティブな連想配列を定義
-☑️ ToDoリストを一覧表示するUIを作成
-☑️ ToDoを追加するUIを作成
-☑️ ToDoを追加する関数を作成
-☑️ ToDoを編集するUIを作成
-☑️ ToDoを編集する関数を作成
+☑️ ToDoリストの型定義
+☑️ ToDoリストをリアクティブな連想配列として定義
+☑️ ToDoを一覧表示するUIを作成
+☑️ 新しいToDoを追加するUIを作成
+☑️ 新しいToDoを追加する関数を作成
+☑️ ToDoの完了状態を変更するUIを作成
+☑️ ToDoの完了状態を変更する関数を作成
 ☑️ ToDoを削除するUIを作成
 ☑️ ToDoを削除する関数を作成
-☑️ ToDoの完了状態を切り替えるUIを作成
-☑️ ToDoの完了状態を切り返える関数を作成
-・ToDoを検索するUIを作成
-・ToDoを検索する関数を作成
+☑️ ToDoを編集するUIを作成
+☑️ ToDoを編集する関数を作成
+☑️ ToDoを検索するUIを作成
+☑️ ToDoを検索する関数を作成
 */
+
 "use client";
 import { useState } from "react";
 
+// ToDoリストの型定義
 type ToDoList = {
   id: number;
   action: string;
@@ -23,25 +26,25 @@ type ToDoList = {
 };
 
 export default function ToDoPage() {
+  // ToDoリストを保持するリアクティブな連想配列を定義
   const [todos, setTodos] = useState<ToDoList[]>([]);
 
-  // ToDoを追加する関数
-  /* TODO: eventの型を適切なものに修正する */
-  const addTodo = (event: any) => {
+  // 新しいToDoを追加する関数
+  const addTodo = (event: React.FormEvent<HTMLFormElement>) => {
     // デフォルトのイベントを中断
     event.preventDefault();
 
     // 送信されたデータを定義
-    const submitedData = new FormData(event.target);
+    const submitedData = new FormData(event.currentTarget);
     const submitedAction = submitedData.get("action");
     const submitedLimit = submitedData.get("limit");
 
-    // 送信された値が文字列かどうか検証
+    // 送信されたデータが文字列かどうか検証
     if (typeof submitedAction !== "string" || typeof submitedLimit !== "string") {
       return;
     }
 
-    // 送信された値が空白の場合は処理を中断
+    // 送信されたデータが空でないか検証
     if (submitedAction.length === 0 || submitedLimit.length === 0) {
       return;
     }
@@ -54,70 +57,19 @@ export default function ToDoPage() {
       completed: false,
     };
 
-    // 現在のToDoリストに新しいToDoを追加
-    const newToDoList = todos.concat(newTodo);
-    setTodos(newToDoList);
+    // 現在のToDoListに新しく追加するToDoを連結
+    const newTodos = todos.concat(newTodo);
+    setTodos(newTodos);
 
-    // フォーム送信後は入力された値をリセット
-    event.target.reset();
+    // 送信後は入力された値をリセット
+    event.currentTarget.reset();
   };
 
-  // ToDoを編集する関数
-  /* TODO: eventの型を適切なものに修正する */
-  const editTodo = (event: any, id: number) => {
-    // デフォルトのイベントを中断
-    event.preventDefault();
-
-    // 送信されたデータを定義
-    const submitedData = new FormData(event.target);
-    const submitedAction = submitedData.get("edit-action");
-    const submitedLimit = submitedData.get("edit-limit");
-
-    // 送信された値が文字列かどうか検証
-    if (typeof submitedAction !== "string" || typeof submitedLimit !== "string") {
-      return;
-    }
-
-    // 送信された値が空白の場合は処理を中断
-    if (submitedAction.length === 0 || submitedLimit.length === 0) {
-      return;
-    }
-
-    // 編集対象として渡ってきたidでカラムを特定し上書き
-    setTodos(
-      todos.map((item) => {
-        if (id === item.id) {
-          item.action = submitedAction;
-          item.limit = new Date(submitedLimit);
-          return item;
-        } else {
-          return item;
-        }
-      })
-    );
-
-    // フォーム送信後は入力された値をリセット
-    event.target.reset();
-  };
-
-  // ToDoを削除する関数
-  const deleteTodo = (id: number) => {
-    setTodos(
-      todos.filter((item) => {
-        if (item.id === id) {
-          return false;
-        } else {
-          return true;
-        }
-      })
-    );
-  };
-
-  // ToDoの完了状態を切り替える関数
+  // ToDoの完了状態を変更する関数
   const toggleCompleted = (id: number) => {
     setTodos(
       todos.map((item) => {
-        if (item.id === id) {
+        if (id === item.id) {
           item.completed = !item.completed;
           return item;
         } else {
@@ -127,37 +79,86 @@ export default function ToDoPage() {
     );
   };
 
-  // 検索前のToDoを保持する連想配列を定義
-  const [originalTodos, setOriginalTodos] = useState<ToDoList[]>([]);
+  // ToDoを削除する関数
+  const deleteTodo = (id: number) => {
+    setTodos(
+      todos.filter((item) => {
+        if (id === item.id) {
+          return false;
+        } else {
+          return true;
+        }
+      })
+    );
+  };
 
-  // ToDoを検索する関数
-  /* TODO: eventの型を適切なものに修正する */
-  const searchTodo = (event: any) => {
+  // ToDoを編集する関数
+  const editTodo = (event: React.FormEvent<HTMLFormElement>, id: number) => {
     // デフォルトのイベントを中断
     event.preventDefault();
 
     // 送信されたデータを定義
-    const submitedData = new FormData(event.target);
+    const submitedData = new FormData(event.currentTarget);
+    const submitedAction = submitedData.get("edit-action");
+    const submitedLimit = submitedData.get("edit-limit");
+
+    // 送信されたデータが文字列かどうか検証
+    if (typeof submitedAction !== "string" || typeof submitedLimit !== "string") {
+      return;
+    }
+
+    // 送信されたデータが空でないか検証
+    if (submitedAction.length === 0 || submitedLimit.length === 0) {
+      return;
+    }
+
+    // 編集したデータをToDoListに反映
+    setTodos(
+      todos.map((item) => {
+        if (item.id === id) {
+          item.action = submitedAction;
+          item.limit = new Date(submitedLimit);
+          return item;
+        } else {
+          return item;
+        }
+      })
+    );
+
+    // 送信後は入力された値をリセット
+    event.currentTarget.reset();
+  };
+
+  // 検索前のToDoListを保持する
+  const [originalTodos, setOriginalTodos] = useState<ToDoList[]>([]);
+  // ToDoを検索する関数
+  const searchTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    // デフォルトのイベントを中断
+    event.preventDefault();
+
+    // 送信されたデータを定義
+    const submitedData = new FormData(event.currentTarget);
     const submitedSearchWord = submitedData.get("search");
 
-    // 送信された値が文字列かどうか検証
+    // 送信されたデータが文字列かどうか検証
     if (typeof submitedSearchWord !== "string") {
       return;
     }
 
-    // 送信された値が空白の場合は処理を中断
+    // 送信されたデータが空でないか検証
     if (submitedSearchWord.length === 0) {
       return;
     }
 
-    // 検索前のToDoリストを保持
-    // 検索ボタンを連打しても、最初の1回目でしかtodosの保持は実行されないようにする
-    if (originalTodos.length > 0) {
+    // 検索ボタンを連打しても重複して登録されないよう、oroginalTodosに値があるか検証
+    if (originalTodos.length !== 0) {
       return;
     }
+
+    // 検索前のToDoListを保持
     setOriginalTodos(todos);
 
-    // todosのデータをitemのactionに検索ワードを含むカラムのみにする
+    // ToDoListを検索結果で上書き
     setTodos(
       todos.filter((item) => {
         if (item.action.includes(submitedSearchWord)) {
@@ -171,31 +172,29 @@ export default function ToDoPage() {
 
   // ToDoの検索結果をリセットする関数
   const resetSearch = () => {
-    if (originalTodos.length === 0) {
+    // 検索前にリセットボタンを押していないか検証
+    if (originalTodos.length > 0) {
+      // 検索前のTodoをToDoListに還元する
+      setTodos(originalTodos);
+
+      // 検索実行時にoriginalTodosが0であることを判定するために空に戻す
+      setOriginalTodos([]);
       return;
     }
-    setTodos(originalTodos);
-
-    // originalTodosを空にする
-    setOriginalTodos([]);
   };
 
   return (
     <div>
       <search>
         <form onSubmit={searchTodo}>
-          <dt>
-            <input type="text" name="search" />
-          </dt>
-          <dd>
-            <button type="submit">検索する</button>
-            <button type="button" onClick={resetSearch}>
-              検索結果をリセットする
-            </button>
-          </dd>
+          <input type="text" name="search" id="search" />
+          <button type="submit">検索する</button>
+          <button type="button" onClick={resetSearch}>
+            検索をリセットする
+          </button>
         </form>
       </search>
-      {todos.length === 0 && <p>登録されたToDoはありません。</p>}
+      {todos.length === 0 && <p>登録されたToDoはありません</p>}
       {todos.length > 0 && (
         <ul>
           {todos.map((item) => {
@@ -203,9 +202,7 @@ export default function ToDoPage() {
               <li key={item.id}>
                 <span style={{ textDecoration: item.completed ? "line-through" : "none" }}>
                   <span>{item.action}</span>
-                  <span>
-                    <time>{item.limit.toLocaleDateString()}</time>まで
-                  </span>
+                  <span>{item.limit.toLocaleDateString()}まで</span>
                 </span>
                 <button
                   type="button"
@@ -233,7 +230,7 @@ export default function ToDoPage() {
                     <dl>
                       <div>
                         <dt>
-                          <label htmlFor="edit-action">内容を編集する</label>
+                          <label htmlFor="edit-action">内容を編集</label>
                         </dt>
                         <dd>
                           <input type="text" name="edit-action" id="edit-action" />
@@ -241,7 +238,7 @@ export default function ToDoPage() {
                       </div>
                       <div>
                         <dt>
-                          <label htmlFor="edit-limit">期限を編集する</label>
+                          <label htmlFor="edit-limit">期限を編集</label>
                         </dt>
                         <dd>
                           <input type="date" name="edit-limit" id="edit-limit" />
