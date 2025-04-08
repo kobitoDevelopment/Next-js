cf2dd115779d79533bafb5ae863b30158bfc9588
I_AM_NOT_SPAM <95177138+kobitoDevelopment@users.noreply.github.com>
2025-04-08 15:47:56 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false, // useEffectやuseMemoが2回実行される問題を解消するためにfalseに設定
+  reactStrictMode: true, // useEffectやuseMemoが2回実行されるときはstrictModeが起因しているが、flaseにすると警告や勧告が表示されなくなるためtrue
 };
 
 export default nextConfig;
