66250837461a263fb984efe2d332b976d2ad6419
I_AM_NOT_SPAM <95177138+kobitoDevelopment@users.noreply.github.com>
2025-06-11 10:16:11 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,18 @@ import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { LoadingProvider } from "@/app/context/LoadingContext";
 import "./globals.css";
+import { DotGothic16 } from "next/font/google"; // GoogleFontsを使用する場合のimport例
+
+// Google FontsのDotGothic16フォントを読み込む
+// ここでは、フォントのweightやsubsetsを指定して、カスタマイズ可能
+// 変数名を指定することで、CSSでフォントを簡単に使用できるようにする
+// 例: font-family: var(--font-dotgothic);
+const dotGothic = DotGothic16({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dotgothic", // フォント変数名を定義
+});
 
 export default function RootLayout({
   children,
@@ -16,7 +28,7 @@ export default function RootLayout({
     console.log(pathname);
   }, [pathname]); // ページ遷移時に発火
   return (
-    <html lang="ja">
+    <html lang="ja" className={dotGothic.variable}>
       <body>
         <LoadingProvider>{children}</LoadingProvider>
       </body>
